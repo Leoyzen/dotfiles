53ffb76c60a6c40f0434fd9f8eb491173b833294
Yuchen Liu <leoyzen@yuchenliudeMacBook-Pro.local>
2017-04-17 12:39:29 +0800

    Add Machine Learning and Pyenv Fish Enviroment

---
+++ home/.config/fish/conf.d/ml.fish
@@ -0,0 +1,4 @@
+set -x KERAS_BACKEND tensorflow
+set -x GLOG_minloglevel 3
+set -x TF_CPP_MIN_LOG_LEVEL 3
+set -x CUDA_VISIBLE_DEVICES 1
---
+++ home/.config/fish/conf.d/pyenv.virtualenv.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -x PYENV_ROOT $HOME/.pyenv
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x PYTHON_BUILD_MIRROR_URL http://mirrors.sohu.com/python/
+
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
