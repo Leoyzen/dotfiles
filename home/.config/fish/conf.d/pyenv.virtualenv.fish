17904f3fa483a6964f36b2dc05535a0ecbf2cb7b
Yuchen Liu <yuchen.liu@yitu-inc.com>
2017-08-11 14:56:32 +0800

    New pyenv enviroments

--- home/.config/fish/conf.d/pyenv.virtualenv.fish
+++ home/.config/fish/conf.d/pyenv.virtualenv.fish
@@ -3,5 +3,5 @@ set -x PYENV_ROOT $HOME/.pyenv
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x PYTHON_BUILD_MIRROR_URL http://mirrors.sohu.com/python/
 
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+status --is-interactive; and source < (pyenv init -|psub)
+status --is-interactive; and source < (pyenv virtualenv-init -|psub)
