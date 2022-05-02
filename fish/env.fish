9fe9305bfc74ce17135b13c68298be2b34a4ee7b
Leoyzen <leoyzen@gmail.com>
2022-05-02 22:57:55 +0800

    update

--- fish/env.fish
+++ fish/env.fish
@@ -1,2 +1,4 @@
 set -g fish_term24bit 1
 set -gx PIER_CONFIG_PATH $HOME/.config/pier/pier.toml
+set -gx HOMEBREW_BREW_GIT_REMOTE https://mirrors.ustc.edu.cn/brew.git
+set -gx FZF_DEFAULT_COMMAND "fd --type file --color=always"
