f0ac8703e4dba41492839ce02f151e2a3e9e830f
Lucian Liu <yuchen.liu@Mac.leoyzen.vip>
2026-01-18 02:18:30 +0800

    优化 dotter 配置：支持跨平台和不同用户名
    主要改进：
    - 添加 macos.toml 和 linux.toml 平台特定配置模板
    - Fish shell 配置使用 test (uname) 检测平台并自动设置路径
    - 支持不同平台选择性地启用/禁用包
    - 修复 local.toml 的 packages 数组格式问题
    - 添加平台优化文档

--- config/shell/fish/fish/brew.fish
+++ config/shell/fish/fish/brew.fish
@@ -1,7 +1,20 @@
-set -x HOMEBREW_PREFIX $HOME/.linuxbrew
-set -x HOMEBREW_CELLAR $HOME/.linuxbrew/Cellar
-set -x HOMEBREW_REPOSITORY $HOME/.linuxbrew/Homebrew
-set -x MANPATH $HOME/.linuxbrew/share/man $MANPATH
-set -x INFOPATH $HOME/.linuxbrew/share/info $INFOPATH
+# 根据平台设置 Homebrew 路径
+if test (uname) = "Darwin"
+    set -x HOMEBREW_PREFIX /opt/homebrew
+    set -x HOMEBREW_CELLAR /opt/homebrew/Cellar
+    set -x HOMEBREW_REPOSITORY /opt/homebrew
+else
+    set -x HOMEBREW_PREFIX $HOME/.linuxbrew
+    set -x HOMEBREW_CELLAR $HOME/.linuxbrew/Cellar
+    set -x HOMEBREW_REPOSITORY $HOME/.linuxbrew/Homebrew
+end
 
-set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/linuxbrew-bottles
+set -x MANPATH $HOMEBREW_PREFIX/share/man $MANPATH
+set -x INFOPATH $HOMEBREW_PREFIX/share/info $INFOPATH
+
+# 镜像源
+if test (uname) = "Darwin"
+    set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+else
+    set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/linuxbrew-bottles
+end
--- config/shell/fish/fish/env.fish
+++ config/shell/fish/fish/env.fish
@@ -1,5 +1,13 @@
 set -g fish_term24bit 1
-set -gx EDITOR /opt/homebrew/bin/code
+
+# 根据平台设置编辑器
+if test (uname) = "Darwin"
+    set -gx EDITOR /opt/homebrew/bin/code
+else
+    set -gx EDITOR vim
+end
+
+# 环境变量
 set -gx PIER_CONFIG_PATH $HOME/.config/pier/pier.toml
 set -gx HOMEBREW_BREW_GIT_REMOTE https://mirrors.ustc.edu.cn/brew.git
 set -gx FZF_DEFAULT_COMMAND "fd --type file --color=always"
